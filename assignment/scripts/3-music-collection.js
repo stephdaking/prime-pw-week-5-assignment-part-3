90ebb379b87a671a9169bcad9777072e5f94f58f
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-05-26 17:37:29 -0500

    made empty album object, addToCollection function, executed the addToCollection function, add testing console logs

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,21 @@
 console.log('***** Music Collection *****');
 
 let collection = [];
+let album = {};
+
+const addToCollection = (title, artist, yearPublished) => {
+	album = {
+		title: title,
+		artist: artist,
+		yearPublished: yearPublished
+	};
+	collection.push(album);
+	return album;
+};
+
+addToCollection('TitleName', 'FakeArtist', 1997);
+console.log(album);
+console.log(collection);
+addToCollection('Album2', 'Akon', 2025);
+console.log(album);
+console.log(collection);
